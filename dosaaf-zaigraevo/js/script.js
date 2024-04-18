520a0b91068d81e8b7e5555809f6821d39f2d11e
InternetFog <antoha_iv@list.ru>
2024-04-18 19:05:18 +0800

    Layouts debug

--- dosaaf-zaigraevo/js/script.js
+++ dosaaf-zaigraevo/js/script.js
@@ -1,5 +1,27 @@
+// Navigation opening & Header floating
 const navBtn = document.querySelector('.nav__btn');
-if(!!navBtn) navBtn.addEventListener('click', ()=>{
+const header = document.querySelector('.header');
+if (!!navBtn) navBtn.addEventListener('click', ()=>{
 	navBtn.classList.toggle("is--opened");
 	document.querySelector('.nav__menu--hidden').classList.toggle('is--opened');
 })
+
+if (!!header && !!navBtn) {
+	let last_scroll = 0;
+	window.addEventListener('scroll', ()=>{
+		window.scrollY > last_scroll && !navBtn.classList.contains("is--opened")
+		? header.classList.add('is--hiding')
+		: header.classList.remove('is--hiding')
+		last_scroll = window.scrollY;
+	});
+}
+
+
+// Courses Slider
+const coursesArr = document.querySelectorAll('#courses-section .courses')
+// if (!!coursesArr.length) {
+// 	window.addEventListener('scroll', ()=> {
+// 		document.getElementById('showScroll').innerHTML = pageYOffset + 'px';
+
+// 	});
+// }
