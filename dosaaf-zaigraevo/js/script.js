f08388987faf4f3878162c75741d3cd01c2292ac
InternetFog <antoha_iv@list.ru>
2024-04-20 09:55:03 +0800

    Scroll slider is ready

--- dosaaf-zaigraevo/js/script.js
+++ dosaaf-zaigraevo/js/script.js
@@ -30,10 +30,30 @@ if (!!header && !!navBtn) {
 
 
 // Courses Slider
+const coursesWrap = document.querySelector('#courses-section .secrion__scroll-wrapper');
 const coursesArr = document.querySelectorAll('#courses-section .courses')
-// if (!!coursesArr.length) {
-// 	window.addEventListener('scroll', ()=> {
-// 		document.getElementById('showScroll').innerHTML = pageYOffset + 'px';
+if (!!coursesWrap && !!coursesArr.length) {
+	let lastScrollTop = 0;
+	coursesWrap.addEventListener('scroll', ()=> {
+		let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		for (let i=0; i<coursesArr.length; i++) {
+			if (coursesWrap.getBoundingClientRect().y == coursesArr[i].getBoundingClientRect().y ) {
+				let scrollTop = coursesArr[0].getBoundingClientRect().y;
+				if (scrollTop > lastScrollTop) { // Scroll-up
+					if(i!==0) coursesArr[i-1].classList.add('is--active')
 
-// 	});
-// }
+					coursesArr[i].classList.add('is--active')
+					coursesArr[i].classList.remove('is--prev')
+
+					if(i!==coursesArr.length-1) coursesArr[i+1].classList.remove('is--active')
+				} else { // Scroll-down
+					if(i!==0) coursesArr[i-1].classList.remove('is--active')
+					if(i!==0) coursesArr[i-1].classList.add('is--prev')
+
+					coursesArr[i].classList.add('is--active')
+				}
+				lastScrollTop = scrollTop
+			}
+		}
+	});
+}
